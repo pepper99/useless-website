88baa957370f9e7358cd3e139fe2a0e1213c4f33
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-01-30 01:19:25 +0700

    Adding pages head

--- pages/fact/index.js
+++ pages/fact/index.js
@@ -1,19 +1,29 @@
+import Head from "next/head";
+
 export default function Fact({ fact, source }) {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center p-2 dark:bg-zinc-900">
-      <div className="rounded-md container max-w-lg mx-auto p-4 shadow-lg dark:bg-zinc-800 hover:scale-110 transition-transform duration-200">
-        <h1 className="italic select-none dark:text-zinc-400">
-          And your useless fact is ..
-        </h1>
-        <p className="font-bold text-xl text-center m-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-500 selection:bg-purple-400 selection:text-white">
-          {fact}
-        </p>
-        <p className="text-xs text-right text-pink-400 hover:text-pink-500 hover:underline transition-colors select-none"><a href={source}>Source</a></p>
+    <>
+      <Head>
+        <title>Random useless fact</title>
+        <meta charset="UTF-8"/>
+        <meta name="description" content="Get your random useless fact"/>
+        <meta name="author" content="Poptum Charoennaew"/>
+      </Head>
+      <div className="w-screen h-screen flex flex-col items-center justify-center p-2 dark:bg-zinc-900">
+        <div className="rounded-md container max-w-lg mx-auto p-4 shadow-lg dark:bg-zinc-800 hover:scale-110 transition-transform duration-200">
+          <h1 className="italic select-none dark:text-zinc-400">
+            And your useless fact is ..
+          </h1>
+          <p className="font-bold text-xl text-center m-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-500 selection:bg-purple-400 selection:text-white">
+            {fact}
+          </p>
+          <p className="text-xs text-right text-pink-400 hover:text-pink-500 hover:underline transition-colors select-none"><a href={source}>Source</a></p>
+        </div>
+        <span className="text-xs dark:text-zinc-600 absolute bottom-1 select-none">
+          Yay
+        </span>
       </div>
-      <span className="text-xs dark:text-zinc-600 absolute bottom-1 select-none">
-        Yay
-      </span>
-    </div>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,36 @@
 import { faBook, faSmileBeam } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Head from "next/head";
 
 export default function Home() {
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center p-2 dark:bg-zinc-900">
-      <div className="flex flex-col items-center rounded-md container max-w-lg mx-auto p-4 shadow-lg dark:bg-zinc-800">
-        <h1 className="text-3xl text-left font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-purple-500 select-none">Welcome to useless website...</h1>
-        <p className="text-left dark:text-white select-none">This is where you read something while you slacking off.</p>
-        <p className="text-left dark:text-white select-none">Hope you enjoy, I guess?</p>
-        
-        <div className="flex flex-col items-center mt-4 space-y-2">
-          <a href="./joke" className="rounded-full font-bold text-center text-md p-1 w-52 bg-pink-400 hover:bg-pink-600 hover:scale-110 transition duration-200 text-white">
-          <FontAwesomeIcon icon={faSmileBeam}/> Jokes
-          </a>
-          <a href="./fact" className="rounded-full font-bold text-center text-md p-1 w-52 bg-pink-400 hover:bg-pink-600 hover:scale-110 transition duration-200 text-white">
-          <FontAwesomeIcon icon={faBook}/> Useless Facts
-          </a>
+    <>
+      <Head>
+        <title>Useless website...</title>
+        <meta charset="UTF-8"/>
+        <meta name="description" content="Get your doses of useless short reading material"/>
+        <meta name="author" content="Poptum Charoennaew"/>
+      </Head>
+      <div className="w-screen h-screen flex flex-col items-center justify-center p-2 dark:bg-zinc-900">
+        <div className="flex flex-col items-center rounded-md container max-w-lg mx-auto p-4 shadow-lg dark:bg-zinc-800">
+          <h1 className="text-3xl text-left font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-purple-500 select-none">Welcome to useless website...</h1>
+          <p className="text-left dark:text-white select-none">This is where you read something while you slacking off.</p>
+          <p className="text-left dark:text-white select-none">Hope you enjoy, I guess?</p>
+          
+          <div className="flex flex-col items-center mt-4 space-y-2">
+            <a href="./joke" className="rounded-full font-bold text-center text-md p-1 w-52 bg-pink-400 hover:bg-pink-600 hover:scale-110 transition duration-200 text-white">
+            <FontAwesomeIcon icon={faSmileBeam}/> Jokes
+            </a>
+            <a href="./fact" className="rounded-full font-bold text-center text-md p-1 w-52 bg-pink-400 hover:bg-pink-600 hover:scale-110 transition duration-200 text-white">
+            <FontAwesomeIcon icon={faBook}/> Useless Facts
+            </a>
+          </div>
+          {/* <button className="rounded-full font-bold text-md p-1 w-24 bg-pink-400 hover:bg-pink-600 transition-colors duration-200 dark:text-white">
+            test
+          </button> */}
         </div>
-        {/* <button className="rounded-full font-bold text-md p-1 w-24 bg-pink-400 hover:bg-pink-600 transition-colors duration-200 dark:text-white">
-          test
-        </button> */}
       </div>
-    </div>
+    </>
   );
 }
