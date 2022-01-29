a4b27dfecf420ca49717b69fcd84b0b6b750c4b2
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-01-30 03:50:50 +0700

    Add dark mode toggle option

--- pages/fact/index.js
+++ pages/fact/index.js
@@ -1,13 +1,24 @@
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import OvalButton from "../../components/OvalButton";
+import useDarkMode from "use-dark-mode";
 import Head from "next/head";
+import useMounted from "../../hooks/useMounted";
 
 export default function Fact({ fact, source }) {
+  const darkMode = useDarkMode(false, {
+    classNameDark: "dark",
+    classNameLight: "light",
+  });
+  const mounted = useMounted();
+
   return (
     <>
       <Head>
         <title>Random useless fact</title>
-        <meta charset="UTF-8"/>
-        <meta name="description" content="Get your random useless fact"/>
-        <meta name="author" content="Poptum Charoennaew"/>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Get your random useless fact" />
+        <meta name="author" content="Poptum Charoennaew" />
       </Head>
       <div className="w-screen h-screen flex flex-col items-center justify-center p-2 dark:bg-zinc-900">
         <div className="rounded-md container max-w-lg mx-auto p-4 shadow-lg dark:bg-zinc-800 hover:scale-110 transition-transform duration-200">
@@ -17,8 +28,18 @@ export default function Fact({ fact, source }) {
           <p className="font-bold text-xl text-center m-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-500 selection:bg-purple-400 selection:text-white">
             {fact}
           </p>
-          <p className="text-xs text-right text-pink-400 hover:text-pink-500 hover:underline transition-colors select-none"><a href={source}>Source</a></p>
+          <p className="text-xs text-right text-pink-400 hover:text-pink-500 hover:underline transition-colors select-none">
+            <a href={source}>Source</a>
+          </p>
         </div>
+        {mounted && (
+          <OvalButton
+            className="absolute bottom-[5%] right-[5%]"
+            onClick={darkMode.toggle}
+          >
+            <FontAwesomeIcon icon={darkMode.value ? faMoon : faSun} />
+          </OvalButton>
+        )}
         <span className="text-xs dark:text-zinc-600 absolute bottom-1 select-none">
           Yay
         </span>
@@ -28,15 +49,15 @@ export default function Fact({ fact, source }) {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
+  const res = await fetch(
+    "https://uselessfacts.jsph.pl/random.json?language=en"
+  );
   const data = await res.json();
-
-  console.log(data);
 
   return {
     props: {
       fact: data.text,
-      source: data.source_url
+      source: data.source_url,
     },
   };
 }
--- pages/joke/index.js
+++ pages/joke/index.js
@@ -1,13 +1,24 @@
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import OvalButton from "../../components/OvalButton";
+import useDarkMode from "use-dark-mode";
 import Head from "next/head";
+import useMounted from "../../hooks/useMounted";
 
 export default function Joke({ joke, category }) {
+  const darkMode = useDarkMode(false, {
+    classNameDark: "dark",
+    classNameLight: "light",
+  });
+  const mounted = useMounted();
+
   return (
     <>
       <Head>
         <title>Random joke</title>
-        <meta charset="UTF-8"/>
-        <meta name="description" content="Get your random joke"/>
-        <meta name="author" content="Poptum Charoennaew"/>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Get your random joke" />
+        <meta name="author" content="Poptum Charoennaew" />
       </Head>
       <div className="w-screen h-screen flex flex-col items-center justify-center p-2 dark:bg-zinc-900">
         <div className="rounded-md container max-w-lg mx-auto p-4 shadow-lg dark:bg-zinc-800 hover:scale-110 transition-transform duration-200">
@@ -17,13 +28,23 @@ export default function Joke({ joke, category }) {
           <p className="font-bold text-xl text-center m-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-500 selection:bg-purple-400 selection:text-white">
             {joke}
           </p>
-          <p className="text-xs text-right text-gray-400 dark:text-zinc-500 select-none">Category: {category}</p>
+          <p className="text-xs text-right text-gray-400 dark:text-zinc-500 select-none">
+            Category: {category}
+          </p>
         </div>
+        {mounted && (
+          <OvalButton
+            className="absolute bottom-[5%] right-[5%]"
+            onClick={darkMode.toggle}
+          >
+            <FontAwesomeIcon icon={darkMode.value ? faMoon : faSun} />
+          </OvalButton>
+        )}
         <span className="text-xs dark:text-zinc-600 absolute bottom-1 select-none">
           Yay
         </span>
       </div>
-    </>    
+    </>
   );
 }
 
@@ -31,12 +52,10 @@ export async function getServerSideProps(context) {
   const res = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
   const data = await res.json();
 
-  console.log(data);
-
   return {
     props: {
       joke: data.joke,
-      category: data.category
+      category: data.category,
     },
   };
 }
