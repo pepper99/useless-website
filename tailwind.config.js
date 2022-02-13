81dec9d8d6fa6c89cfa106fef6f2f140d39f00e6
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-13 17:55:51 +0700

    Add tailwind shake animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shake: "shake 250ms infinite",
+      },
+      keyframes: {
+        shake: {
+          "10%": {
+            transform: "translateX(-3%)",
+          },
+          "30%": {
+            transform: "translateX(3%)",
+          },
+          "50%": {
+            transform: "translateX(-5%)",
+          },
+          "70%": {
+            transform: "translateX(-5%)",
+          },
+          "90%": {
+            transform: "translateX(-1%)",
+          },
+          "100%": {
+            transform: "translateX(0)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
