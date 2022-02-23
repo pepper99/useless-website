dc90c5217579d6dde80a573b017a208b68b20162
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-24 00:48:46 +0700

    Add nextjs progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import NextNProgress from "nextjs-progressbar";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-
   return (
+    <>
+      <NextNProgress color="#ec4899"/>
       <Component {...pageProps} />
+    </>
   );
 }
 
